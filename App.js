4fbf3d7cf9204902b31298d8ca50d931a2ff9666
mtintti <tiina.mantykivi@gmail.com>
2024-10-11 13:34:55 +0300

    tehty

--- App.js
+++ App.js
@@ -1,11 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useReducer } from 'react';
+import { StyleSheet, Text, View, FlatList, Pressable, TextInput, TouchableOpacity } from 'react-native';
+import { collection, getDocs, doc, setDoc, deleteDoc } from "firebase/firestore";
+import { db } from './fireBaseConfig';
+
+function todosReducer(state, action) {
+  switch (action.type) {
+    case "SET":
+      return action.payload;
+    case "ADD":
+      return [...state, { text: action.text, completed: false, id: action.id }];
+    case "TOGGLE":
+      return state.map((todo, index) =>
+        index === action.index ? { ...todo, completed: !todo.completed } : todo
+      );
+    case "REMOVE":
+      return state.filter((_, index) => index !== action.index);
+    default:
+      throw new Error();
+  }
+}
 
 export default function App() {
+  const [text, setText] = useState(""); 
+  const [tasks, dispatch] = useReducer(todosReducer, []);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        console.log("Fetching...");
+        const querySnapshot = await getDocs(collection(db, "List"));
+        const todosFromFirestore = querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        dispatch({ type: "SET", payload: todosFromFirestore });
+        console.log("Fetching Done");
+        console.log(tasks)
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  async function addTodo() {
+    console.log("Adding to database...");
+    if (text.trim()) {
+      const docRef = doc(collection(db, "List"));
+      await setDoc(docRef, { name: text });
+      dispatch({ type: "ADD", name: text, id: docRef.id });
+      setText("");
+      console.log("adding Done");
+    }
+  }
+
+  async function removeTodo(index, id) {
+    dispatch({ type: 'REMOVE', index });
+    await deleteDoc(doc(db, "List", id));
+  }
+
+  const renderItem = ({ item, index }) => (
+    <TouchableOpacity onLongPress={() => removeTodo(index, item.id)}>
+      <Text style={[styles.todoItem, item.completed && styles.completed]}>
+        {item.name}
+      </Text>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 24 }}>Shopping List</Text>
+      <TextInput
+        value={text}
+        onChangeText={setText}
+        placeholder="Add new task..."
+        style={styles.input}
+      />
+      <Pressable onPress={addTodo}>
+        <Text style={{color: "blue"}}>Save</Text>
+      </Pressable>
+      
+      <FlatList
+        data={tasks}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
@@ -14,7 +94,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 20,
+  },
+  todoItem: {
+    padding: 15,
+    fontSize: 18,
+  },
+  completed: {
+    textDecorationLine: 'line-through',
+    color: 'gray',
   },
 });
---
+++ fireBaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_AjJaWkf65qm8m3t3ixFbGhf67Wnd2nI",
+  authDomain: "shoppinglist-49b99.firebaseapp.com",
+  projectId: "shoppinglist-49b99",
+  storageBucket: "shoppinglist-49b99.appspot.com",
+  messagingSenderId: "812602864156",
+  appId: "1:812602864156:web:4bd90bf8761d9c98d365d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db };
